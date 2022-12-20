08b00206c9d2bd73c8cb7342a5d3600583ccf622
cbowman422 <chrisb0wman@hotmail.com>
2022-12-20 15:22:06 -0500

    added moving loading icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import People from '../pages/People'
-import Show from '../pages/Show'
+import People from '../pages/People/People'
+import Show from '../pages/Show/Show'
 
 const Main = () => {
   return (
